d01d9e5cc3f0e8108b91df6cc21a8549afc79c31
Kikugie <kikugie@duck.com>
2024-06-08 12:11:48 +0200

    feat: add old textures as a built-in resourcepack

--- src/main/kotlin/dev/kikugie/elytratrims/platform/ETClientWrapper.kt
+++ src/main/kotlin/dev/kikugie/elytratrims/platform/ETClientWrapper.kt
@@ -7,15 +7,24 @@ import dev.kikugie.elytratrims.common.ETReference
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType
+import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.resource.ResourceManager
 import net.minecraft.resource.ResourceReloader
 import net.minecraft.resource.ResourceType
+import net.minecraft.text.Text
 import net.minecraft.util.profiler.Profiler
 import java.util.concurrent.Executor
 
 object ETClientWrapper : ClientModInitializer {
     override fun onInitializeClient() {
         ETClient.init()
+        ResourceManagerHelper.registerBuiltinResourcePack(
+            ETReference.id("legacy"),
+            FabricLoader.getInstance().getModContainer(ETReference.MOD_ID).get(),
+            Text.translatable("elytratrims.legacy_rp"),
+            ResourcePackActivationType.NORMAL
+        )
         ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(object :
             IdentifiableResourceReloadListener {
             override fun reload(
